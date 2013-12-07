8b43a0d07c55d86c0edf2b875600e857f34e6f68
Louis Chatriot <louis.chatriot@gmail.com>
2013-12-07 10:39:23 -0800

    Test it works without a callback

--- test/index.test.js
+++ test/index.test.js
@@ -119,7 +119,24 @@ describe('NeDB Logger', function () {
         done();
       });      
     });
-  });    
+  });
+
+  it("Can insert without a callback", function (done) {
+    var logger = new Logger({ filename: testDb });
+        
+    logger.insert({ hello: "world" });
+    setTimeout(function () {
+      assert.isNotNull(fs.readFileSync(testDb, 'utf8').match(/^{"hello":"world","_id":"[a-zA-Z0-9]{16}"}\n$/));
+      
+      db = new Datastore({ filename: testDb, autoload: true });
+      db.find({}, function (err, docs) {
+        assert.isNull(err);
+        docs.length.should.equal(1);
+        docs[0].hello.should.equal("world");
+        done();
+      });
+    }, 100);
+  });
 
 
 });
