50ebff0a6a0c0f88dad1e63bbd93b4a88c688277
joe <Joe vanderstelt>
2017-02-23 11:32:37 -0500

    typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+// Type definitions for NeDB 0.1
+// Project: https://github.com/louischatriot/nedb
+// Definitions by: Joe Vanderstelt <https://github.com/thisboyiscrazy>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = NeDBLogger;
+
+declare class NeDBLogger {
+    constructor(path?: string | {filename: string});
+
+    /**
+     * Insert a new document
+     * @param {Function} cb Optional callback, signature: err, insertedDoc
+     */
+    insert<T>(newDoc: T, cb?: (err: Error, document: T) => void): void;
+
+}
